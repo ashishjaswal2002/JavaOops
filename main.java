b0b3723b150ee4eff4bbc2c40fa7f73feb252603
Ashish jaswal <jaswalashish131@gmail.com>
2024-01-13 18:01:42 +0530

    Inheritance done

--- main.java
+++ main.java
@@ -1,7 +1,40 @@
+
+class Animal{
+
+    Animal(){
+        System.out.println("I am an Animal");
+    }
+
+    Animal(String type){
+        System.out.println("Type"+type);
+    }
+
+
+}
+
+class Dog extends Animal{
+
+
+  Dog(){
+    super("Dog");
+    System.out.println("I am a Dog");
+  }
+
+}
+
 public class main{
 
+     
+    public static void main(String[]args){
+     
+
+        Dog d1 = new Dog();
+
+        
+      
       
 
+    }
 
 
 }
