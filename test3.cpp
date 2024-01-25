60fd7c2cc1e49a9362481e239679c6b26dd36235
Ashish jaswal <jaswalashish131@gmail.com>
2024-01-26 01:32:08 +0530

    added

--- test3.cpp
+++ test3.cpp
@@ -1,28 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void printSubsequence(int i,vector<int>& ans,int arr[],int& n){
+ 
+  
+  if(i==n){
+    for(auto& a : ans){
+      cout<<a<<" ";
+    }
+    if(ans.size()==0){
+      cout<<"{}";
+    }
+    cout<<endl;
+
+    return;
+  }
+
+  ans.push_back(arr[i]);
+
+  printSubsequence(i+1,ans,arr,n);
+  ans.pop_back();
+  printSubsequence(i+1,ans,arr,n);
+
+
+
+}
 int main(){
  
-   vector<int> v;
-   vector<int>v1;
+ 
+ int arr[] ={3,1,2};
+ int n = 3;
+ vector<int> ans;
+printSubsequence(0,ans,arr,n);
 
-   v.push_back(1);
 
-   v.push_back(2);
 
-   v1.push_back(1);
 
-   v1.push_back(3);
-bool areEqual = std::equal(v.begin(), v.end(), v1.begin());
-
-if(areEqual){
-    cout<<"YES"<<endl;
-
-}
-else{
-    cout<<"NO"<<endl;
-}
-
-return 0;
 
 
 }
