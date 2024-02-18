0affde73d990cfa9f0cb35b0061a761c4c19d895
Ashish jaswal <jaswalashish131@gmail.com>
2024-02-18 15:37:01 +0530

    added

--- Borze.cpp
+++ Borze.cpp
@@ -1,45 +1,39 @@
-#include<bits/stdc++.h>
-#define TestCase int t; cin>>t; while(t--)
+#include <bits/stdc++.h>
+#define TestCase \
+  int t;         \
+  cin >> t;      \
+  while (t--)
 using namespace std;
-int main(){
+int main()
+{
+
+  vector<int> arr = {1};
+  int k = 0;
+
+  unordered_map<int, int> mp;
+
+  for (auto x : arr)
+  {
+    mp[x]++;
+  }
+
+  vector<int> v;
+
+  for (auto x : mp)
+  {
+
+    v.push_back(x.second);
+    //{1 4},{1 2},{2 1},{3 3}
+  }
+
+  sort(v.begin(), v.end());
+  for(int i=0;i<v.size();i++)
+  {
+     k-=v[i];
+
+     if(k<0){
+      return v.size()-i;
+     }
+  }
  
-  string zero = ".";
-  string one = "-.";
-  string two ="--";
-
-  string borzeCode;
-  cin>>borzeCode;
-
-   //.-.--
-   // --.
-   // -..-.--
-
-   string ans = "";
-
-   for(int i=0;i<borzeCode.length();){
-     
-      if(borzeCode[i]=='.'){
-         
-        ans+='0';
-        i++;
-      }
-
-       if(borzeCode[i]=='-'){
-          
-          if(borzeCode[i+1]=='.'){
-            ans+='1';
-          }else{
-            ans+='2';
-          }
-          i+=2;
-       }
-
-   }
-
-    cout<<ans<<" \n";
-
-      
-
- 
-
 }
--- Test.cpp
+++ Test.cpp
@@ -1,27 +1,10 @@
 #include<bits/stdc++.h>
 #define TestCase int t; cin>>t; while(t--)
 using namespace std;
-
-class TreeNode{
- public:
-    int data;
-    TreeNode* left;
-    TreeNode* right;
-
-    TreeNode(int data){
-       this->data = data;
-
-     left = NULL;
-     right = NULL;
-    }
-
-};
-
-
 int main(){
+ 
+   int n;
+   cin>>n;
 
-   TreeNode* root = new TreeNode(1);
-   TreeNode* left = new TreeNode(2);
-   TreeNode* right = new TreeNode(3);
 
 }
